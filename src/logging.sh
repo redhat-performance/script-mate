c3536714a18712ec3aed7a114e2e128874b0f72c
Jan Hutar <jhutar@redhat.com>
2025-05-31 17:38:10 +0200

    feat: Colorful log messages

--- src/logging.sh
+++ src/logging.sh
@@ -1,8 +1,21 @@
 # Generic helpers for logging
 
+_YELLOW='\033[1;33m'
+_GREEN='\033[0;32m'
+_ORANGE='\033[0;33m'
+_RED='\033[0;31m'
+_NC='\033[0m'
+
 # Internal: Logging function doing actual output.
 function _log() {
-    echo "$( date -Ins --utc ) $1 $2" >&2
+    case $1 in
+        DEBUG) c=$_YELLOW;;
+        INFO) c=$_GREEN;;
+        WARNING) c=$_ORANGE;;
+        ERROR|FATAL) c=$_RED;;
+        *) c="";;
+    esac
+    echo -e "${c}$( date -Ins --utc ) ${1} ${2}${_NC}" >&2
 }
 
 # Public: Logging function for "debug" level.
